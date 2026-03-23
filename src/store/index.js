d84bab60b7552b2c7274e53d6af99e4febb35326
Sofia <itssonyamisyura@gmail.com>
2026-03-23 17:58:24 +0100

    add enhancers

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,28 @@
-import { legacy_createStore as createStore, combineReducers } from 'redux';
+import { legacy_createStore as createStore, combineReducers, compose } from 'redux';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
 
-const store = createStore( combineReducers({heroes, filters}),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const enhancer = (createStore) => (...atgs) => {
+    const store = createStore(...atgs);
+
+    const oldDispatch = store.dispatch; // link to orig dispatch
+    store.dispatch = (action) => {
+        if (typeof action === 'string') {
+            return oldDispatch({
+                type: action
+            })
+        }
+        return oldDispatch(action)
+    }
+    return store;
+}
+
+const store = createStore( 
+                    combineReducers({heroes, filters}),
+                    compose(
+                        enhancer, 
+                        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+                    ));
 
 export default store;
