4a04a1ec81efb8c130770f0001abb17beb25a826
Sofia <itssonyamisyura@gmail.com>
2026-03-24 15:32:22 +0100

    Redux Toolkit: configureStore()

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,6 @@
-import { legacy_createStore as createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
-import { thunk as ReduxThunk } from "redux-thunk";
 
 const stringMiddleware = () => (next) => (action) => {
     if (typeof action === 'string') {
@@ -27,10 +26,16 @@ const enhancer = (createStore) => (...atgs) => {
     return store;
 }
 
-const store = createStore( 
-                    combineReducers({heroes, filters}),
-                    compose(applyMiddleware(ReduxThunk, stringMiddleware), 
-                        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-                );
+// const store = createStore( 
+//                     combineReducers({heroes, filters}),
+//                     compose(applyMiddleware(ReduxThunk, stringMiddleware), 
+//                         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+//                 );
+
+const store = configureStore({
+    reducer: {heroes, filters},
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware),
+    devTools: process.env.NODE_ENV !== 'production',
+})
 
 export default store;
